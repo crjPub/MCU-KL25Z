c1997abbd14c1bdb83d2567215d93a424ac3b764
crjPub <48241978+crjPub@users.noreply.github.com>
2019-03-24 12:12:30 +0800

    Create example.c

---
+++ 4.PWM/example.c
@@ -0,0 +1,47 @@
+int main(void)
+{
+    bool brightnessUp = true; /* Indicates whether the LED is brighter or dimmer. */
+    tpm_config_t tpmInfo;
+    uint8_t updatedDutycycle = 0U;
+    tpm_chnl_pwm_signal_param_t tpmParam[2];
+    /* Configures the TPM parameters with frequency 24 kHZ. */
+    tpmParam[0].chnlNumber = (tpm_chnl_t)BOARD_FIRST_TPM_CHANNEL;
+    tpmParam[0].level = kTPM_LowTrue;
+    tpmParam[0].dutyCyclePercent = 0U;
+    tpmParam[1].chnlNumber = (tpm_chnl_t)BOARD_SECOND_TPM_CHANNEL;
+    tpmParam[1].level = kTPM_LowTrue;
+    tpmParam[1].dutyCyclePercent = 0U;
+    /* Board pin, clock, and debug console initialization. */
+    BOARD_InitHardware();
+    TPM_GetDefaultConfig(&tpmInfo);
+    /* Initializes the TPM module. */
+    TPM_Init(BOARD_TPM_BASEADDR, &tpmInfo);
+    TPM_SetupPwm(BOARD_TPM_BASEADDR, tpmParam, 2U, kTPM_EdgeAlignedPwm, 24000U, TPM_SOURCE_CLOCK);
+    TPM_StartTimer(BOARD_TPM_BASEADDR, kTPM_SystemClock);
+    while (1)
+    {
+        /* Delays to see the change of LED brightness. */
+        delay();
+        if (brightnessUp)
+        {
+            /* Increases a duty cycle until it reaches a limited value. */
+            if (++updatedDutycycle == 100U)
+            {
+                brightnessUp = false;
+            }
+        }
+        else
+        {
+            /* Decreases a duty cycle until it reaches a limited value. */
+            if (--updatedDutycycle == 0U)
+            {
+                brightnessUp = true;
+            }
+        }
+        /* Starts PWM mode with an updated duty cycle. */
+        TPM_UpdatePwmDutycycle(BOARD_TPM_BASEADDR, (tpm_chnl_t)BOARD_FIRST_TPM_CHANNEL, kTPM_EdgeAlignedPwm,
+                               updatedDutycycle);
+        TPM_UpdatePwmDutycycle(BOARD_TPM_BASEADDR, (tpm_chnl_t)BOARD_SECOND_TPM_CHANNEL, kTPM_EdgeAlignedPwm,
+                               updatedDutycycle);
+    }
+}
